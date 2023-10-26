6f014eaab97b1693d33cfa30a85129ba4ad894cd
IriannaP <iriannapp@gmail.com>
2023-10-26 17:52:30 -0600

    HUD y Pause menu
    Para cada uno hice una escena.
    El HUD lo hice sin el UI Toolkit y le creé su script.

---
+++ Selvmord/Assets/_Scripts/UI/HUD/Life.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Life : MonoBehaviour
+{
+    public Image life_bar;
+    public float life_amount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
