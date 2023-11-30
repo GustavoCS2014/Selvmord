f6a4fdc08f56a30530e4ba09cefa9edcca847723
Gustavo <100179597+GustavoCS2014@users.noreply.github.com>
2023-11-30 17:14:25 -0600

    ohhh yeah, chacarron

--- Selvmord/Assets/_Scripts/Enemies/BulletFlyEnemy.cs
+++ Selvmord/Assets/_Scripts/Enemies/BulletFlyEnemy.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class BulletFlyEnemy : MonoBehaviour
 {
     [SerializeField] float speedBullet;
+    [SerializeField] ParticleSystem ParticleEmitter;
+    private bool crashed;
+    private Rigidbody2D rb2d;
     MainSystem MS;
     
 
     private void Start()
     {
         MS = GameObject.FindGameObjectWithTag("MainSystem").GetComponent<MainSystem>();
-       
+        rb2d = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        if(crashed && ParticleEmitter.isStopped) {
+
+            GetComponent<Collider2D>().enabled = true;
+            GetComponentInChildren<SpriteRenderer>().enabled = true;
+            Destroy(gameObject);
+        }
         transform.Translate(Vector2.right * speedBullet * Time.deltaTime);
-        Invoke("Destroy", 4f);
+        Invoke("Destroy", 3f);
     }
 
     private void Destroy()
@@ -29,13 +38,19 @@ public class BulletFlyEnemy : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Destroy(gameObject);
+            ParticleEmitter.Play();
+            GetComponent<Collider2D>().enabled = false;
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            crashed = true;
         }
 
         if (collision.gameObject.CompareTag("Player"))
         {
+            ParticleEmitter.Play();
+            GetComponent<Collider2D>().enabled = false;
+            GetComponentInChildren<SpriteRenderer>().enabled = false;
+            crashed = true;
             MS.DamagePlayer(30, new Vector2(0,0));
-            Destroy(gameObject);
 
         }
     }
